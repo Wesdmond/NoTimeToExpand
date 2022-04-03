ad1f2271dc5792d3cb3f043de91dd72214625692
Wesdmond <SHto2004@ya.ru>
2022-04-03 19:07:04 +0500

    add AudioManager and some little changes

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public Sound[] sounds;
+
+    internal override void Awake()
+    {
+        base.Awake();
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.pitch = s.pitch;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+        s.source.Stop();
+    }
+
+    public void Restart(string name)
+    {
+        Stop(name);
+        Play(name);
+    }
+}
