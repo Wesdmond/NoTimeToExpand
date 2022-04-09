a69efdb4d41c968099d5450a376f27a381eb7e2b
Wesdmond <SHto2004@ya.ru>
2022-04-09 17:47:15 +0500

    return URP renderer back

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,40 @@ public class PlayerController : MonoBehaviour
 {
 
     public PlayerInput playerInput;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
     Vector3 rawInputMovement;
 
     [Range(0.001f, 0.1f)]
     public float movementSpeed;
 
-    bool isRight = true;
+    bool down;
+
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
+    }
 
     public void OnMovement(InputAction.CallbackContext value)
     {
         Vector2 inputMovement = value.ReadValue<Vector2>();
+
+        animator.SetFloat("Horizontal speed", Mathf.Abs(inputMovement.x));
+        animator.SetFloat("Vertical speed", Mathf.Abs(inputMovement.y));
+
+        if (inputMovement.y < 0)
+            down = true;
+        else if (inputMovement.y > 0)
+            down = false;
+
+        animator.SetBool("Down", down);
+
+        if (inputMovement.x < 0)
+            spriteRenderer.flipX = false;
+        else if (inputMovement.x > 0)
+            spriteRenderer.flipX = true;
+
         rawInputMovement = new Vector3(inputMovement.x, inputMovement.y, 0);
     }
 
